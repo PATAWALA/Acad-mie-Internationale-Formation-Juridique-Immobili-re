3b990838f3d0f9bff4cbb29d0612793fc7faaf07
Patawala <patawalaabdoulaye200@gmail.com>
2026-08-01 13:06:54 +0100

    40 updatre

--- src/components/public/Header.tsx
+++ src/components/public/Header.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 const navLinks = [
   { label: 'Programmes', href: '#certificates' },
   { label: 'Bourse', href: '#registration-form' },
+  { label: 'Témoignages', href: '#social-proof' },
   { label: 'Corps enseignant', href: '#faculty' },
 ];
 
