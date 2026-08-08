ca4a32205ff7138069569bddc1402facdfdf412d
Patawala <patawalaabdoulaye200@gmail.com>
2026-08-08 01:56:01 +0100

    73 update

--- src/app/evenements/layout.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function EventsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-[#020617] text-white">
-      {children}
-    </div>
-  );
-}
